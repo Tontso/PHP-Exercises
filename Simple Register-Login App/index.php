dfd37c49aa4d01f9bc5240d192be0e3b142b9821
Tontso <tommy.tonchev@outlook.com>
2022-02-07 01:01:08 +0200

    Hide database's login info

--- Simple Register-Login App/index.php
+++ Simple Register-Login App/index.php
@@ -4,7 +4,7 @@ spl_autoload_register(function($className){
     require_once $className . '.php';
 });
 
-$pdo = new PDO("mysql:dbname=session_test_db;host=localhost:3306", "root", "");
+$pdo = new PDO("****************************);
 $db = new Database\PDODatabase($pdo);
 
 
